94d0f46667275a91dbd197630d28333fbcd3a609
noobfromph <ejlungay@gmail.com>
2025-08-14 11:32:59 +0800

    fix: send mouseup event

--- drag.cc
+++ drag.cc
@@ -1,5 +1,6 @@
 #include <napi.h>
 #include <windows.h>
+#include <fstream>
 
 Napi::Value StartDrag(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
@@ -9,11 +10,27 @@ Napi::Value StartDrag(const Napi::CallbackInfo& info) {
     return env.Null();
   }
 
-  Napi::Buffer<char> hwndBuffer = info[0].As<Napi::Buffer<char>>();
-  HWND hwnd = *reinterpret_cast<HWND*>(hwndBuffer.Data());
+  HWND hwnd = *reinterpret_cast<HWND*>(info[0].As<Napi::Buffer<char>>().Data());
+
+  POINT startPos;
+  GetCursorPos(&startPos);
+  int startX = startPos.x;
+  int startY = startPos.y;
+
+  ScreenToClient(hwnd, &startPos);
+  LPARAM lParam = MAKELPARAM(startPos.x, startPos.y);
 
   ReleaseCapture();
   SendMessage(hwnd, WM_SYSCOMMAND, SC_MOVE | HTCAPTION, 0);
+
+  POINT endPos;
+  GetCursorPos(&endPos);
+  int endX = endPos.x;
+  int endY = endPos.y;
+
+  if (endX == startX && endY == startY) {
+    SendMessage(hwnd, WM_LBUTTONUP, 0, lParam);
+  }
 
   return env.Null();
 }
